bae0615297f1e53f7fe648ddf8915f30742ad5df
paulakreft <kreftpaulinaa@gmail.com>
2020-07-05 19:38:07 +0200

    Menu nie jest ostylowane

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import Sidebar from './Sidebar'
+import Menu from './Menu'
 
 import './App.css';
 
 function App() {
   return (
     <Container maxWidth="m">
+      <Menu></Menu>
       <Sidebar></Sidebar>
     
     </Container>
