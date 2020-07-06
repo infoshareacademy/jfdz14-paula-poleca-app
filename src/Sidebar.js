8c5ecc55b24f38b1252e96993db8d5d041a044cf
paulakreft <kreftpaulinaa@gmail.com>
2020-07-06 20:44:28 +0200

    template for new sidbar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,48 +1,108 @@
-
-import React, { useState } from "react";
-import Drawer from "@material-ui/core/Drawer";
-import Button from "@material-ui/core/Button";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 
 const Sidebar = () => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleDrawer = () => setIsOpen(!isOpen);
-
-    return (
-        <div>
-            <Button onClick={toggleDrawer}>Menu</Button>
-            <Drawer open={isOpen} onClose={toggleDrawer} anchor="left">
-            <List>
-                <ListItem button onClick={toggleDrawer}>
-                    <ListItemText primary="Statystyki" />
-                </ListItem>
-                
-            <ListItem button onClick={toggleDrawer}>
-                <ListItemText primary="Teatr" />
-            </ListItem>
-            <ListItem button onClick={toggleDrawer}>
-                <ListItemText primary="Kino" />
-            </ListItem>
-            <ListItem button onClick={toggleDrawer}>
-                <ListItemText primary="Muzea" />
-            </ListItem>
-            <ListItem button onClick={toggleDrawer}>
-                <ListItemText primary="Koncert" />
-            </ListItem>
-            <ListItem button onClick={toggleDrawer}>
-                <ListItemText primary="Sport" />
-            </ListItem>
-            </List>
-            </Drawer>
-        </div>
-
-
-    )
+    
 }
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
-export default Sidebar
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={6}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: 224,
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
+export default function VerticalTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        className={classes.tabs}
+      >
+        <Tab label="Item One" {...a11yProps(0)} />
+        <Tab label="Item Two" {...a11yProps(1)} />
+        <Tab label="Item Three" {...a11yProps(2)} />
+        <Tab label="Item Four" {...a11yProps(3)} />
+        <Tab label="Item Five" {...a11yProps(4)} />
+        <Tab label="Item Six" {...a11yProps(5)} />
+        <Tab label="Item Seven" {...a11yProps(6)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Item Four
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Item Five
+      </TabPanel>
+      <TabPanel value={value} index={5}>
+        Item Six
+      </TabPanel>
+      <TabPanel value={value} index={6}>
+        Item Seven
+      </TabPanel>
+    </div>
+  );
+}
