7b840a549fe697282bc70d8384f0cf741c06ed15
Michal <karnath.michal@gmail.com>
2020-07-19 20:55:45 +0200

    dodany bootstrap - zmiana koncepcji, fetch danych

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import Sidebar from './Sidebar'
-import Menu from './Menu'
-import Form from './Modal'
-import Events from './Events'
-
 import './App.css';
+import AppNavbar from './components/Navbar'
+import Events from './Events';
+import Sidebar from './components/Sidebar'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
-    <Container maxWidth="m">
-      {/* <Events /> */}
-      <Sidebar></Sidebar>
-    
+    <Container fluid>
+      <Row>
+        <Col>
+          <AppNavbar />
+          <div style={{display:'flex', float: 'left'}}>
+
+            <Sidebar />
+            <Events />
+          </div>
+        </Col>
+      </Row>
+        {/* <Row>
+        <Col>
+          <AppNavbar />
+          <Row>
+            <Col>
+              <Sidebar />
+            </Col>
+            <Col>
+              <Events />
+            </Col>             
+          </Row>
+
+        </Col>
+      </Row> */}
     </Container>
+    
   );
 }
 
--- src/Events.js
+++ src/Events.js
@@ -1,27 +1,59 @@
 import React from 'react'
-
+import ListGroup from 'react-bootstrap/ListGroup'
 
 
 class Events extends React.Component {
     
-    
-    // fetchEvents() {
-    //     fetch('https://isa-cors-proxy.herokuapp.com/api/rest/events.json')
-    //         .then(response => response.json())
-    //         .then(events => {
-    //            console.log(events)
-    //             })
-    //         }
-    
+    state = {
+        events: []
+    }
 
+    componentDidMount() {
+            fetch(' https://isa.mateuszmarzecki.pl/v1/proxy?url=https://planerkulturalny.pl/api/rest/events.json ')
+                .then(response => response.json())
+                .then(events => {
+                    
+                    this.setState({
+                        events: events
+                    })
+                })
+    }
 
  
 
 
 
     render() {
-        return <h1>Eventy</h1>     
-        
+            return(
+
+                <ListGroup>
+                {
+                    this.state.events
+                    .map((event) => {
+                        
+                        return <ListGroup.Item key = {event.id}>
+                            <div className="movie-card">
+            
+                                <div className="movie-card card">
+                                {/* <img
+                                    src={card}
+                                    alt=""
+                                /> */}
+                                <div>
+                                    <h4>{event.name}</h4>
+                                    <p className="movie-card-description" style={{ fontSize: "14px" }}>
+                                    {event.place.name}
+                                    </p>
+                                </div>
+                                 
+                            
+                            </div>
+                            </div>
+                        </ListGroup.Item>
+                    })
+                }
+                </ListGroup>
+        ); 
     }
 }
 export default Events
