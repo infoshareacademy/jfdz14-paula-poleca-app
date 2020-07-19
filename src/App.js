d97968f7dd2d8bd07036c6fbe31fe7ac4d90aa31
Michal <karnath.michal@gmail.com>
2020-07-19 18:48:58 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Container from '@material-ui/core/Container';
 import Sidebar from './Sidebar'
 import Menu from './Menu'
 import Form from './Modal'
+import Events from './Events'
 
 import './App.css';
 
 function App() {
   return (
     <Container maxWidth="m">
+      {/* <Events /> */}
       <Sidebar></Sidebar>
     
     </Container>
---
+++ src/Events.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+
+class Events extends React.Component {
+    
+    
+    // fetchEvents() {
+    //     fetch('https://isa-cors-proxy.herokuapp.com/api/rest/events.json')
+    //         .then(response => response.json())
+    //         .then(events => {
+    //            console.log(events)
+    //             })
+    //         }
+    
+
+
+ 
+
+
+
+    render() {
+        return <h1>Eventy</h1>     
+        
+    }
+}
+export default Events
