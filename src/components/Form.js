04f03c39a71da0dd8a1587c8d026a248beb4b912
paulakreft <kreftpaulinaa@gmail.com>
2020-07-22 21:59:46 +0200

    test

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
-const Form = () => {
+const Forms = () => {
     return (
-        <div>
-            <h2>Form</h2>
-            <div>for Paula</div>
-        </div>
-    );
+    <Form>
+        <Form.Group controlId="exampleForm.ControlInput1">
+            <Form.Label>Nazwa wydarzenia</Form.Label>
+            <Form.Control type="text" placeholder="Tytuł"  isRequired/>
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlSelect1">
+            <Form.Label>Wybierz miasto</Form.Label>
+            <Form.Control as="select" isRequired>
+            <option>Gdańsk</option>
+            <option>Gdynia</option>
+            <option>Sopot</option>
+            </Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Opis wydarzenia: </Form.Label>
+            <Form.Control as="textarea" rows="3"isRequired />
+        </Form.Group>
+        <Form.Group>
+            <Form.File id="exampleFormControlFile1" label="Example file input" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+        Zapisz
+        </Button>
+    </Form>
+            );
 }
  
-export default Form;
+export default Forms;
+
+
