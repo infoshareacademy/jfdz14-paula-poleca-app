0552ef4b6d0dfc702c5b2929ba676d4a6ba3c8b5
paulakreft <kreftpaulinaa@gmail.com>
2020-07-27 19:34:56 +0200

    data save in localstorage

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,50 +2,82 @@ import React from 'react';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-const Forms = () => {
-    return (
-    <Form>
-        <div>
-            <h2>Dodaj nowe wydarzenie</h2>
-            <p>Chcesz się podzielić z innymi nadchodzącycm wydarzeniem? Znasz miejsce, cene i godzinę? Dodaj nowe wydarzenie do PaulaPoleca!</p></div>
-        <Form.Group controlId="exampleForm.ControlInput1">
-            <Form.Label>Nazwa wydarzenia</Form.Label>
-            <Form.Control type="text" placeholder="Tytuł"  isRequired/>
-        </Form.Group>
+class Forms extends React.Component {
+        state = {
+          text: " ",
+          city: "Gdańsk"
+        };
+      
+        keyUpHandler = (event) => {
+          console.log(event.target.value);
+      
+          this.setState({
+            text: event.target.value,
+          })
+        }
 
-        <Form.Group controlId="exampleForm.ControlInput1">
-            <Form.Label>Cena</Form.Label>
-            <Form.Control type="number" placeholder="PLN"  isRequired/>
-        </Form.Group>
+        handlerOnChange = (event) =>{
+            console.log(event.target.value)
 
-        <Form.Group controlId="exampleForm.ControlSelect1">
-            <Form.Label>Wybierz miasto</Form.Label>
-            <Form.Control as="select" isRequired>
-            <option>Gdańsk</option>
-            <option>Gdynia</option>
-            <option>Sopot</option>
-            </Form.Control>
-        </Form.Group>
-        <Form.Group>
-            <Form.File id="exampleFormControlFile1" label="Example file input" />
-        </Form.Group>
+            this.setState({
+                city: event.target.city
+            })
+            
+        }
 
-        <Form.Group controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Opis wydarzenia: </Form.Label>
-            <Form.Control as="textarea" rows="3"isRequired />
-        </Form.Group>
-        <Form.Group>
-        <Form.Label>Link do wydarzenia</Form.Label>
-            <Form.Control type="text" placeholder="Link do wydarzenia"/>
-        </Form.Group>
+        saveDataInLolcalStorage = () => {
+            localStorage.setItem("new_event", "{}")
+        }
 
-        <Button variant="primary" type="submit">
-        Zapisz
-        </Button>
-    </Form>
+
+    render(){
+        return (
+            <div className="my-3">
+            <Form>
+                    <h2>Dodaj nowe wydarzenie</h2>
+                    <p>Chcesz się podzielić z innymi nadchodzącycm wydarzeniem? Znasz miejsce, cene i godzinę? Dodaj nowe wydarzenie do PaulaPoleca!</p>
+                <Form.Group controlId="exampleForm.ControlInput1">
+                    <Form.Label>Nazwa wydarzenia</Form.Label>
+                    <Form.Control type="text" onKeyUp={this.keyUpHandler} placeholder={"Tytuł" }/>
+                </Form.Group>
+        
+                <Form.Group controlId="exampleForm.ControlInput1">
+                    <Form.Label>Cena</Form.Label>
+                    <Form.Control type="number" onKeyUp={this.keyUpHandler} placeholder="PLN" />
+                </Form.Group>
+        
+                <Form.Group controlId="exampleForm.ControlSelect1">
+                    <Form.Label >Wybierz miasto</Form.Label>
+                    <Form.Control as="select" onChange={this.handlerOnChange} >
+                    <option>Gdańsk</option>
+                    <option>Gdynia</option>
+                    <option>Sopot</option>
+                    </Form.Control>
+                </Form.Group>
+                <Form.Group>
+                    <Form.File id="exampleFormControlFile1" label="Example file input" />
+                </Form.Group>
+        
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Opis wydarzenia: </Form.Label>
+                    <Form.Control as="textarea" rows="3" />
+                </Form.Group>
+                <Form.Group>
+                <Form.Label>Link do wydarzenia</Form.Label>
+                    <Form.Control type="text" placeholder="Link do wydarzenia"/>
+                </Form.Group>
+        
+                <Button variant="primary" type="submit" onClick={this.saveDataInLolcalStorage}>
+                Zapisz
+                </Button>
+            </Form>
+            </div>
+        
             );
-}
- 
+          }
+
+    }
+
+    
+
 export default Forms;
-
-
