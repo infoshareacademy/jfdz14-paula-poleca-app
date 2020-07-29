f9433d254f94188d0c53b750559ca0542f39f2f5
Andrzej Stasiński <andis_as@wp.pl>
2020-07-29 16:33:33 +0200

    Updates

---
+++ precache-manifest.7f62bf4cc14d1c02179fcd4f12c8cb47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c28388225d101394eafd5bef9a3656",
+    "url": "/jfdz14-paula-poleca-app/index.html"
+  },
+  {
+    "revision": "d3971a6e8395ea8246be",
+    "url": "/jfdz14-paula-poleca-app/static/css/main.4ab89168.chunk.css"
+  },
+  {
+    "revision": "75f425329e1dd36cd1b9",
+    "url": "/jfdz14-paula-poleca-app/static/js/2.8ce5292a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/jfdz14-paula-poleca-app/static/js/2.8ce5292a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3971a6e8395ea8246be",
+    "url": "/jfdz14-paula-poleca-app/static/js/main.ad0f4916.chunk.js"
+  },
+  {
+    "revision": "bfe53469c176f22cf9c9",
+    "url": "/jfdz14-paula-poleca-app/static/js/runtime-main.29aed46b.js"
+  },
+  {
+    "revision": "8f2c3aec37e20a84ea3f3a212a8c03b5",
+    "url": "/jfdz14-paula-poleca-app/static/media/gdansk.8f2c3aec.jpg"
+  }
+]);
