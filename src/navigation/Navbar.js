63210cac6b5be24ef5999b8adab62c5fbbf723f4
paulakreft <kreftpaulinaa@gmail.com>
2020-07-29 10:15:37 +0200

    blue color of navbar

--- src/navigation/Navbar.js
+++ src/navigation/Navbar.js
@@ -3,32 +3,31 @@ import { NavLink } from 'react-router-dom';
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import '../styles/Navbar.css';
+// import logo from "./logo.png";
+
+
+
 
 const AppNavbar = () => {
 
     return(
-        <div style={{paddingLeft:"0", paddingRight:"0"}}>
-            <Navbar className="mr-auto" bg="light" variant="light">
-            <Navbar.Brand className="navbar" to="/">
-                      <img 
-                      alt=""
-                      src="../img/logo-white.png"
-                      width="30"
-                      height="30"
-                      className="d-inline-block align-top"
+            <Navbar bg="primary" variant="dark">
+                <Navbar.Brand href="/">
+                <img
+                    alt=""
+                    // src={logo}
+                    width="32"
+                    height="32"
+                    className="d-inline-block align-top"
+                />{' '}
+                Paula Poleca
+                </Navbar.Brand>
 
-                    />
-                    Paula Poleca</Navbar.Brand>
-            <Nav className="mr-auto">
-                {/* <Nav.Link as={NavLink} to="/events">Events</Nav.Link>
-                <Nav.Link as={NavLink} to="/addEvent">Dodaj event</Nav.Link>
-                <Nav.Link as={NavLink} to="/favourite">Favourite</Nav.Link> */}
-            </Nav>
             </Navbar>
-        </div>
 
 
         );    
 }
 
 export default AppNavbar
+ 
