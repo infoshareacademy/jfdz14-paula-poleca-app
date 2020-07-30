6c9f163f214b275393248bdf47f8e1dd4225cc35
paulakreft <kreftpaulinaa@gmail.com>
2020-07-30 23:34:21 +0200

    try to fix

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -44,3 +44,66 @@ const EventModal = () => {
 
 
 
+
+  
+//   class EventModal extends Component {
+//       constructor(props) {
+//           super(props);
+//           this.state = {
+//             isOpen: false,
+//             setIsOpen: false,
+
+
+//           }
+//       }
+
+
+//     showModal = () => {
+//         this.setState({
+//             setIsOpen: true
+//     })};
+    
+//     hideModal = () => {
+//         this.setState({
+//             setIsOpen: false
+//     })};
+
+
+
+//       render() { 
+//           return (
+//               <>
+//               {
+//                   this.props.events
+//                   .map((event) => {
+  
+//                       return (
+//                         <div>
+//                         <Button onClick={this.showModal} style={{textAlign:"center"}}>Dowiedz się więcej</Button>
+//                         <Modal show={this.isOpen} onHide={this.hideModal} >
+//                             <Modal.Header>
+//                             <Modal.Title>{event.name}</Modal.Title>
+//                         </Modal.Header>
+//                         <Modal.Body>
+//                             <div>{event.startDate.slice(0, 10)}</div>
+//                             <div> {event.descLong} </div>
+//                         </Modal.Body>
+//                         <Modal.Footer>
+//                             <Button variant="danger" onClick={this.hideModal}>Zamknij</Button>
+//                             <Button  variant="success" onClick={this.hideModal}>Przejdz do Wydarzenia</Button>
+                
+//                           </Modal.Footer>
+                
+//                         </Modal>
+
+
+//                         </div>  
+//                       );
+//                   })
+//               }
+//               </>
+//           );
+//       }
+//   }
+   
+//   export default EventModal;
