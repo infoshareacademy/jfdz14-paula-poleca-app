7177f4f6c75043da04f5cd08aa20483ed57b2014
Volodymyr Hannibal <v.m.hannibal@gmail.com>
2020-08-02 14:33:46 +0200

    Updates

---
+++ precache-manifest.788d3f30b954d71cf04f9213a605fb85.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d497ff358344bd79a85c23deb5fd1e6",
+    "url": "/jfdz14-paula-poleca-app/index.html"
+  },
+  {
+    "revision": "503b5c9ec8b5c0abd0aa",
+    "url": "/jfdz14-paula-poleca-app/static/css/main.82fdc115.chunk.css"
+  },
+  {
+    "revision": "008f7dd90f5b04a6c830",
+    "url": "/jfdz14-paula-poleca-app/static/js/2.0c6f8787.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/jfdz14-paula-poleca-app/static/js/2.0c6f8787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "503b5c9ec8b5c0abd0aa",
+    "url": "/jfdz14-paula-poleca-app/static/js/main.4a226711.chunk.js"
+  },
+  {
+    "revision": "bfe53469c176f22cf9c9",
+    "url": "/jfdz14-paula-poleca-app/static/js/runtime-main.29aed46b.js"
+  },
+  {
+    "revision": "8f2c3aec37e20a84ea3f3a212a8c03b5",
+    "url": "/jfdz14-paula-poleca-app/static/media/gdansk.8f2c3aec.jpg"
+  },
+  {
+    "revision": "8f2c3aec37e20a84ea3f3a212a8c03b5",
+    "url": "/jfdz14-paula-poleca-app/static/media/gdansk1.8f2c3aec.jpg"
+  },
+  {
+    "revision": "395935ed554826593c11de723ead1041",
+    "url": "/jfdz14-paula-poleca-app/static/media/logo-white.395935ed.png"
+  }
+]);
