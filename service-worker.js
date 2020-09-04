c6ba4427aa472a04fbc01c953715d4a7f10be63c
Andrzej Stasiński <andis_as@wp.pl>
2020-09-04 21:46:17 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jfdz14-paula-poleca-app/precache-manifest.fc2dbe4e48bb590804609070ca5601b1.js"
+  "/jfdz14-paula-poleca-app/precache-manifest.ea8a33cb1e2d03274f4de041750c92e9.js"
 );
 
 self.addEventListener('message', (event) => {
